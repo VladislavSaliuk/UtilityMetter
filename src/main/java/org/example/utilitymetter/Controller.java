e70a7774e4ea095bd67a47901a70ab033056ed78
Vladislav Saliuk <salukvladislav81@gmail.com>
2024-02-28 20:43:01 +0200

    Adding controller

--- src/main/java/org/example/utilitymetter/Controller.java
+++ src/main/java/org/example/utilitymetter/Controller.java
@@ -1,5 +1,30 @@
 package org.example.utilitymetter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Button addToHistoryButton;
+
+    @FXML
+    private Button calculateBillButton;
+
+    @FXML
+    private TextField currentIndicatorTextField;
+
+    @FXML
+    private TextField previousIndicatorTextField;
+
+    @FXML
+    private TextField tariffTextField;
+
+    @FXML
+    private ChoiceBox<String> utilityChoiseBox;
+
+
 
 }
