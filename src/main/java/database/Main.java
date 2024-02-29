c7f537b2c42eee1462aa1984e0da16250cdca835
Vladislav Saliuk <salukvladislav81@gmail.com>
2024-02-29 19:53:27 +0200

    Provide connection toMongoDB Database

---
+++ src/main/java/database/Main.java
@@ -0,0 +1,11 @@
+package database;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoDatabase;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.utilitymetter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires mongo.java.driver;
 
 
     opens org.example.utilitymetter to javafx.fxml;
