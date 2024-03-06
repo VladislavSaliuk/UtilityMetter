2ccb549e5b991b34cfcc7f9e93e58f96337bce74
Vladislav Saliuk <salukvladislav81@gmail.com>
2024-03-06 18:10:37 +0200

    Adding beautiful dashboard and styles

--- src/main/java/home/controllers/Controller.java
+++ src/main/java/home/controllers/Controller.java
@@ -1,23 +1,61 @@
 package home.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
     private Stage stage;
     @FXML
+    private Label paneLabel;
+    @FXML
     private AnchorPane scenePane;
     @FXML
-    private Label exitLabel;
+    private Pane pane;
     @FXML
-    void initialize(){
-        exitLabel.setOnMouseClicked(event -> {
-            stage = (Stage) scenePane.getScene().getWindow();
-            stage.close();
-        });
+    private Button calculateBillButton;
+    @FXML
+    private Button countersButton;
+    @FXML
+    private Button historyButton;
+    @FXML
+    private Button exitButton;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+
+    @FXML
+    private void handleClicks(ActionEvent event){
+        if(event.getSource() == calculateBillButton) {
+            paneLabel.setText("Calculate bill");
+            pane.setBackground(new Background(new BackgroundFill(Color.rgb(89,107,124), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+        else if(event.getSource() == countersButton) {
+            paneLabel.setText("Counters");
+            pane.setBackground(new Background(new BackgroundFill(Color.rgb(93,124,89), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+        else if(event.getSource() == historyButton) {
+            paneLabel.setText("History");
+            pane.setBackground(new Background(new BackgroundFill(Color.rgb(124,85,124), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+    }
+
+    public void handleClose(ActionEvent event){
+        if(event.getSource() == exitButton){
+            System.exit(0);
+        }
+    }
+
 
 }
