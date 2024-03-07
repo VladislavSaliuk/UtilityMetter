7dce9a07efacb5b93ee9b212cd7ce7aae99a67c3
Vladislav Saliuk <salukvladislav81@gmail.com>
2024-03-07 15:05:46 +0200

    Adding all panels and styles!

--- src/main/java/home/controllers/Controller.java
+++ src/main/java/home/controllers/Controller.java
@@ -6,6 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -30,24 +32,43 @@ public class Controller implements Initializable {
     private Button historyButton;
     @FXML
     private Button exitButton;
+    @FXML
+    private GridPane calculateBillGridPane;
+    @FXML
+    private GridPane countersGridPane;
+    @FXML
+    private GridPane historyGridPane;
+    @FXML
+    private TableView countersTableView;
+    @FXML
+    private TableView historyTableView;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        setTableColumnHeaderColor(countersTableView, "#5D7C59FF");
+        setTableColumnHeaderColor(historyTableView, "#7C557CFF");
     }
 
+    private void setTableColumnHeaderColor(TableView<?> tableView, String color) {
+        for (TableColumn<?, ?> column : tableView.getColumns()) {
+            column.setStyle("-fx-background-color: " + color + ";");
+        }
+    }
     @FXML
     private void handleClicks(ActionEvent event){
         if(event.getSource() == calculateBillButton) {
             paneLabel.setText("Calculate bill");
             pane.setBackground(new Background(new BackgroundFill(Color.rgb(89,107,124), CornerRadii.EMPTY, Insets.EMPTY)));
+            calculateBillGridPane.toFront();
         }
         else if(event.getSource() == countersButton) {
             paneLabel.setText("Counters");
             pane.setBackground(new Background(new BackgroundFill(Color.rgb(93,124,89), CornerRadii.EMPTY, Insets.EMPTY)));
+            countersGridPane.toFront();
         }
         else if(event.getSource() == historyButton) {
             paneLabel.setText("History");
             pane.setBackground(new Background(new BackgroundFill(Color.rgb(124,85,124), CornerRadii.EMPTY, Insets.EMPTY)));
+            historyGridPane.toFront();
         }
     }
 
