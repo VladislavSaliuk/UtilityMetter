c83e486ca13f84796be94c0d5a8f136ebd81d7a9
Vladislav Saliuk <salukvladislav81@gmail.com>
2024-03-11 09:10:38 +0200

    Adding view for adding counter

--- src/main/java/home/controllers/CountersController.java
+++ src/main/java/home/controllers/CountersController.java
@@ -1,15 +1,29 @@
 package home.controllers;
 
+import home.UtilityMetterApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+
 
 public class CountersController {
     @FXML
     private TableView countersTableView;
+
+    @FXML
+    public void addCounterButton(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(UtilityMetterApplication.class.getResource("add-counter.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 300);
+        stage.setTitle("Utility metter");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
